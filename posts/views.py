719884cfe59eab22f462a5a616782b48a3f1a9ec
Lora Griffin <lora.jean@me.com>
2017-12-07 15:35:01 -0600

    setting up posts view step 1

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+# view handles the http request and returns a response
+
+
+def posts_home(request):
+
+    return HttpResponse("<h1>Hello</h1")
