a465877efd691c73fe663982327ecd5c76291d01
Lora Griffin <lora.jean@me.com>
2017-12-11 10:10:28 -0600

    creating dynamic urls

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list),
     path('create/', views.post_create),
-    path('detail/', views.post_detail),
+    path('(<id>\d+)/', views.post_detail),
     path('update/', views.post_update),
     path('delete/', views.post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+from .models import Post
+
 # view handles the http request and returns a response
 
 
@@ -8,12 +10,27 @@ def post_create(request):
     return HttpResponse("<h1>Create</h1")
 
 
-def post_detail(request):
-    return HttpResponse("<h1>Detail</h1")
+def post_detail(request, id):
+    instance = get_object_or_404(Post, id=id)
+    context = {
+        "title": instance.title,
+        "instance": instance
+    }
+    return render(request, "post_detail.html", context)
 
 
 def post_list(request):
-    return HttpResponse("<h1>List</h1")
+    queryset = Post.objects.all()
+    # if request.user.is_authenticated():
+    #     context = {
+    #         "title": "My User List"
+    #     }
+    # else:
+    context = {
+        "object_list": queryset,
+        "title": "List"
+    }
+    return render(request, "index.html", context)
 
 
 def post_update(request):
