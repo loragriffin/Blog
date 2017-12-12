652645709cc6689bb62e85a02f577ff25a273e07
Lora Griffin <lora.jean@me.com>
2017-12-11 20:07:10 -0600

    added successful messages

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
 
 from .models import Post
 from .forms import PostForm
@@ -11,7 +12,11 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Successfully Created")
         return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, "Not Successfully Created")
+
     context = {
         "form": form,
     }
@@ -48,6 +53,7 @@ def post_update(request, id):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, "Saved")
         return HttpResponseRedirect(instance.get_absolute_url())
 
     context = {
