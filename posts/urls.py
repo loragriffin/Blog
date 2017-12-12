fcaba320fe70d1ee2c3e1f52eb3ce035ac766d66
Lora Griffin <lora.jean@me.com>
2017-12-11 19:30:43 -0600

    added edit functionality

--- posts/urls.py
+++ posts/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('', views.post_list),
     path('create/', views.post_create),
     path('<int:id>/', views.post_detail, name='detail'),
-    path('update/', views.post_update),
+    path('<int:id>/edit/', views.post_update, name='update'),
     path('delete/', views.post_delete),
 ]
