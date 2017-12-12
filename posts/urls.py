a8b703caeae5855e73edef8835fcad57f3214c0b
Lora Griffin <lora.jean@me.com>
2017-12-11 20:14:52 -0600

    added delete function

--- posts/urls.py
+++ posts/urls.py
@@ -4,9 +4,9 @@ from . import views
 
 app_name = 'posts'
 urlpatterns = [
-    path('', views.post_list),
+    path('', views.post_list, name='list'),
     path('create/', views.post_create),
     path('<int:id>/', views.post_detail, name='detail'),
     path('<int:id>/edit/', views.post_update, name='update'),
-    path('delete/', views.post_delete),
+    path('<int:id>/delete/', views.post_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse, HttpResponseRedirect
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 
 from .models import Post
@@ -64,5 +64,8 @@ def post_update(request, id):
     return render(request, "post_form.html", context)
 
 
-def post_delete(request):
-    return HttpResponse("<h1>Delete</h1")
+def post_delete(request, id):
+    instance = get_object_or_404(Post, id=id)
+    instance.delete()
+    messages.success(request, "Post deleted")
+    return redirect("posts:list")
